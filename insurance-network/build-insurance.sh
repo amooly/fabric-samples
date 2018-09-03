a49dfd0390303b5d2896583be1925c039bafe62f
墨黎 <haibin.whb@alibaba-inc.com>
2018-09-03 14:45:55 +0800

    保险区块链平台基础配置

---
+++ insurance-network/build-insurance.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export FABRIC_CFG_PATH=$PWD
+
+## 生成证书
+cryptogen generate --config=./insurance-crypto-config.yaml
+
+## 生成创世区块
+configtxgen -profile InsuranceOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+
+## 生成保险业务渠道
+configtxgen -profile InsuranceOrgsChannel -outputCreateChannelTx ./channel-artifacts/insuranceChannel.tx -channelID insurance
+
+## 生成基础信息渠道
+configtxgen -profile FundamentalOrgsChannel -outputCreateChannelTx ./channel-artifacts/fundamentalChannel.tx -channelID fundamental
