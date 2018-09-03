cec9950ccb8f237746964cf812ce74f663254ac3
墨黎 <haibin.whb@alibaba-inc.com>
2018-09-03 19:13:51 +0800

    修改脚本

--- insurance-network/build-insurance.sh
+++ insurance-network/build-insurance.sh
@@ -2,16 +2,16 @@
 export FABRIC_CFG_PATH=$PWD
 
 ## 生成证书
-cryptogen generate --config=./insurance-crypto-config.yaml
+sudo cryptogen generate --config=./insurance-crypto-config.yaml
 
 ## 生成创世区块
-configtxgen -profile InsuranceOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+sudo configtxgen -profile InsuranceOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
 ## 生成保险业务渠道
-configtxgen -profile InsuranceOrgsChannel -outputCreateChannelTx ./channel-artifacts/insuranceChannel.tx -channelID insurance
+sudo configtxgen -profile InsuranceOrgsChannel -outputCreateChannelTx ./channel-artifacts/insuranceChannel.tx -channelID insurance
 
 ## 生成基础信息渠道
-configtxgen -profile FundamentalOrgsChannel -outputCreateChannelTx ./channel-artifacts/fundamentalChannel.tx -channelID fundamental
+sudo configtxgen -profile FundamentalOrgsChannel -outputCreateChannelTx ./channel-artifacts/fundamentalChannel.tx -channelID fundamental
 
 # 启动
 docker-compose -f docker-compose-insurance.yaml up -d
